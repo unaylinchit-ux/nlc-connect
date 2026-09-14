b384a5f81e35d43feae36b8380eed9bcabddc62f
unaylinchit-ux <unaylinchit@gmail.com>
2026-09-14 12:28:10 +0700

    Create firebase.js

---
+++ sr/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, OAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUMMY-KEY-REPLACE-LATER",
+  authDomain: "nlc-connect.firebaseapp.com",
+  projectId: "nlc-connect",
+  storageBucket: "nlc-connect.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
+export const appleProvider = new OAuthProvider('apple.com');
